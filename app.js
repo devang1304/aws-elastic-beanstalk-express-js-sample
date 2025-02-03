114223a5577bda0c1d803ad3e25a4a650303516f
dd9098 <dd9098@rit.edu>
2025-02-03 13:02:28 -0500

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! This is R2D2 speaking!'));
+app.get('/', (req, res) => res.send('Hello World! This is R2D2 speaking! Who are you my lord?'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
